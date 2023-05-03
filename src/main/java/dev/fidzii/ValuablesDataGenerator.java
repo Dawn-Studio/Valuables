ec021d8c5e2b770e181f14f4d13114e7bf90fd35
Filip Michalski 229959 <229959@edu.p.lodz.pl>
2023-05-03 19:13:04 +0200

    init

---
+++ src/main/java/dev/fidzii/ValuablesDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.fidzii;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ValuablesDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
